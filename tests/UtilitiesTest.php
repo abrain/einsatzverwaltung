e78a2fba806acc26710b2ec4c19a4462c0ab7471
Andreas Brain <dev@abrain.de>
2016-01-20 22:26:17 +0100

    Aufruf der Factory kompatibel zu WordPress vor Version 4.4 gemacht

--- tests/UtilitiesTest.php
+++ tests/UtilitiesTest.php
@@ -27,8 +27,8 @@ class UtilitiesTest extends WP_UnitTestCase
     public function testAddPostToCategory()
     {
         // Einsatzbericht anlegen und mehreren Kategorien zuweisen
-        $postId = $this->factory()->post->create(array('post_type' => 'einsatz'));
-        $categoryIds = $this->factory()->category->create_many(4);
+        $postId = $this->factory->post->create(array('post_type' => 'einsatz'));
+        $categoryIds = $this->factory->category->create_many(4);
         $initialCategories = array_slice($categoryIds, 0, 2);
         wp_set_post_categories($postId, $initialCategories);
         $this->assertEquals($initialCategories, wp_get_post_categories($postId));
@@ -45,8 +45,8 @@ class UtilitiesTest extends WP_UnitTestCase
     public function testRemovePostFromCategory()
     {
         // Einsatzbericht anlegen und allen Kategorien zuweisen
-        $postId = $this->factory()->post->create(array('post_type' => 'einsatz'));
-        $categoryIds = $this->factory()->category->create_many(4);
+        $postId = $this->factory->post->create(array('post_type' => 'einsatz'));
+        $categoryIds = $this->factory->category->create_many(4);
         wp_set_post_categories($postId, $categoryIds);
         $this->assertEquals($categoryIds, wp_get_post_categories($postId));
 

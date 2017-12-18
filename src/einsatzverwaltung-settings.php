563bce4c7c7abe123c2a0df5569c90218fc80e06
Andreas Brain <dev@abrain.de>
2017-12-18 23:16:14 +0100

    Neue Seite About bzw. Kontakt überarbeitet

--- src/einsatzverwaltung-settings.php
+++ src/einsatzverwaltung-settings.php
@@ -738,21 +738,25 @@ class Settings
 
         if ('about' === $currentTab) {
             ?>
-            <h2>Entwicklerkontakt &amp; Social Media</h2>
-            <p>
-                eMail: <a href="mailto:kontakt@abrain.de">kontakt@abrain.de</a>
-                <span title="PGP Schl&uuml;ssel-ID: 8752EB8F" class="pgpbadge"><i class="fa fa-lock"></i>&nbsp;PGP</span>
-            </p>
-            <p align="center">
-                <a href="https://github.com/abrain/einsatzverwaltung/issues" title="Bring Dich ein auf GitHub">
-                    <i class="fa fa-github fa-2x"></i></a>&nbsp;&nbsp;
-                <a href="https://www.facebook.com/einsatzverwaltung/" title="Einsatzverwaltung auf Facebook">
-                    <i class="fa fa-facebook-official fa-2x"></i></a>&nbsp;&nbsp;
-                <a href="https://twitter.com/einsatzvw" title="Einsatzverwaltung auf Twitter">
-                    <i class="fa fa-twitter fa-2x"></i></a>&nbsp;&nbsp;
-                <a href="https://einsatzverwaltung.abrain.de/feed/" title="RSS-Feed mit Neuigkeiten zu Einsatzverwaltung">
-                    <i class="fa fa-rss-square fa-2x"></i></a>
-            </p>
+            <div class="aboutpage-icons">
+                <div class="aboutpage-icon"><a href="https://einsatzverwaltung.abrain.de" target="_blank"><i class="fa fa-globe fa-4x"></i><br>Webseite</a></div>
+                <div class="aboutpage-icon"><a href="https://einsatzverwaltung.abrain.de/dokumentation/" target="_blank"><i class="fa fa-book fa-4x"></i><br>Dokumentation</a></div>
+                <div class="aboutpage-icon"><a href="https://github.com/abrain/einsatzverwaltung" target="_blank"><i class="fa fa-github fa-4x"></i><br>GitHub</a></div>
+                <div class="aboutpage-icon"><a href="https://de.wordpress.org/plugins/einsatzverwaltung/" target="_blank"><i class="fa fa-wordpress fa-4x"></i><br>Plugin-Verzeichnis</a></div>
+            </div>
+
+            <h2>Support</h2>
+            <p>Solltest Du ein Problem bei der Benutzung des Plugin haben, schaue bitte erst auf <a href="https://github.com/abrain/einsatzverwaltung/issues">GitHub</a> und im <a href="https://wordpress.org/support/plugin/einsatzverwaltung">Forum auf wordpress.org</a> nach, ob andere das Problem auch haben bzw. hatten. Wenn es noch keinen passenden Eintrag gibt, lege bitte einen entsprechenden Issue bzw. Thread an. Du kannst aber auch einfach eine <a href="mailto:kontakt@abrain.de">E-Mail</a> schreiben.</p>
+            <p>Gib bitte immer die folgenden Versionen mit an:&nbsp;<code style="border: 1px solid grey;">
+            <?php printf('Plugin: %s, WordPress: %s, PHP: %s', Core::VERSION, get_bloginfo('version'), phpversion()); ?>
+            </code></p>
+
+            <h2>Social Media</h2>
+            <ul>
+                <li>Facebook: <a href="https://www.facebook.com/einsatzverwaltung/" title="Einsatzverwaltung auf Facebook">Einsatzverwaltung</a></li>
+                <li>Twitter: <a href="https://twitter.com/einsatzvw" title="Einsatzverwaltung auf Twitter">@einsatzvw</a></li>
+            </ul>
+            <p>Du kannst die Neuigkeiten auch mit deinem Feedreader abonnieren: <a href="https://einsatzverwaltung.abrain.de/feed/">RSS</a> / <a href="https://einsatzverwaltung.abrain.de/feed/atom/">Atom</a></p>
             <?php
             return;
         }

093978e6c8862158691a1dd293e01c39174fc5f9
Andreas Brain <dev@abrain.de>
2018-05-27 20:59:27 +0200

    Unterseite "Berechtigungen" wiederhergestellt

--- src/Settings/Pages/Capabilities.php
+++ src/Settings/Pages/Capabilities.php
@@ -16,7 +16,13 @@ class Capabilities extends SubPage
 
     public function addSettingsFields()
     {
-        // TODO: Implement addSettingsFields() method.
+        add_settings_field(
+            'einsatzvw_settings_caps_roles',
+            'Rollen',
+            array($this, 'echoFieldRoles'),
+            $this->settingsApiPage,
+            'einsatzvw_settings_caps'
+        );
     }
 
     public function addSettingsSections()
@@ -31,8 +37,50 @@ class Capabilities extends SubPage
         );
     }
 
+    /**
+     * Gibt die Einstellmöglichkeiten für die Berechtigungen aus
+     */
+    public function echoFieldRoles()
+    {
+        echo '<fieldset>';
+        $roles = get_editable_roles();
+        if (empty($roles)) {
+            echo "Es konnten keine Rollen gefunden werden.";
+        } else {
+            foreach ($roles as $roleSlug => $role) {
+                // Administratoren haben immer Zugriff, deshalb ist keine Einstellung nötig
+                if ('administrator' === $roleSlug) {
+                    continue;
+                }
+
+                $this->echoSettingsCheckbox(
+                    'einsatzvw_cap_roles_' . $roleSlug,
+                    translate_user_role($role['name'])
+                );
+                echo '<br>';
+            }
+            echo '<p class="description">Die Benutzer mit den hier ausgew&auml;hlten Rollen haben alle Rechte, um die Einsatzberichte und die zugeh&ouml;rigen Eigenschaften (z.B. Einsatzarten) zu verwalten. Zu dieser Einstellungsseite und den Werkzeugen haben in jedem Fall nur Administratoren Zugang.</p>';
+            echo '<p class="description">Die Berechtigungen können mit speziellen Plugins deutlich feingranularer eingestellt werden.</p>';
+        }
+        echo '</fieldset>';
+    }
+
     public function registerSettings()
     {
-        // TODO: Implement registerSettings() method.
+        $roles = get_editable_roles();
+        if (!empty($roles)) {
+            foreach (array_keys($roles) as $roleSlug) {
+                // Administratoren haben immer Zugriff, deshalb ist keine Einstellung nötig
+                if ('administrator' === $roleSlug) {
+                    continue;
+                }
+
+                register_setting(
+                    'einsatzvw_settings_capabilities',
+                    'einsatzvw_cap_roles_' . $roleSlug,
+                    array(self::$utilities, 'sanitizeCheckbox')
+                );
+            }
+        }
     }
 }

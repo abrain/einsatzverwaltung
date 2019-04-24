8947893b226a9c58e645ce91b2b10ad82680a163
Andreas Brain <dev@abrain.de>
2019-04-24 15:23:11 +0200

    Platzhalter %units% für Verwendung im Widget freigeschalten

--- src/Widgets/RecentIncidentsFormatted.php
+++ src/Widgets/RecentIncidentsFormatted.php
@@ -120,7 +120,8 @@ class RecentIncidentsFormatted extends WP_Widget
     );
     private $allowedTagsPattern = array('%title%', '%date%', '%time%', '%location%', '%duration%',
         '%incidentCommander%', '%incidentType%', '%incidentTypeColor%', '%url%', '%number%', '%seqNum%',
-        '%annotations%', '%vehicles%', '%additionalForces%', '%typesOfAlerting%', '%featuredImage%', '%workforce%');
+        '%annotations%', '%vehicles%', '%units%', '%additionalForces%', '%typesOfAlerting%', '%featuredImage%',
+        '%workforce%');
     private $allowedTagsAfter = array('%feedUrl%');
 
     /**

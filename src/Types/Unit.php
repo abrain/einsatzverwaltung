672cb4f7881c3b83f7debc3d38c94da8788305c3
Andreas Brain <dev@abrain.de>
2019-05-01 19:57:27 +0200

    REST-Endpunkte für Einheiten deaktiviert Bisher sind die Daten auch nicht anderweitig öffentlich, es soll kein Datenleck entstehen. Dadurch kann vorerst nur der klassische Editor für Einheiten benutzt werden.

--- src/Types/Unit.php
+++ src/Types/Unit.php
@@ -58,7 +58,7 @@ class Unit implements CustomPostType
             'show_ui' => true,
             'show_in_menu' => 'edit.php?post_type=' . Report::SLUG,
             'show_in_admin_bar' => false,
-            'show_in_rest' => true,
+            'show_in_rest' => false,
             'capability_type' => $this->getSlug(),
             'map_meta_cap' => true,
             'menu_icon' => 'dashicons-networking',

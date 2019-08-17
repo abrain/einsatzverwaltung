52110565dd2e351b6c0674d289f3cec517d21ea9
Andreas Brain <dev@abrain.de>
2019-08-17 21:27:34 +0200

    Better distinguish between callback for a hook and internal functionality

--- src/ReportNumberController.php
+++ src/ReportNumberController.php
@@ -3,6 +3,17 @@
 namespace abrain\Einsatzverwaltung;
 
 use abrain\Einsatzverwaltung\Model\IncidentReport;
+use function date_create;
+use function date_format;
+use function get_option;
+use function get_post_field;
+use function get_post_type;
+use function in_array;
+use function intval;
+use function is_numeric;
+use function sprintf;
+use function str_pad;
+use function update_post_meta;
 
 /**
  * Takes care of keeping report numbers up-to-date
@@ -17,10 +28,8 @@ class ReportNumberController
      */
     public function __construct()
     {
-        if (self::isAutoIncidentNumbers()) {
-            add_action('updated_postmeta', array($this, 'adjustIncidentNumber'), 10, 4);
-            add_action('added_post_meta', array($this, 'adjustIncidentNumber'), 10, 4);
-        }
+        add_action('updated_postmeta', array($this, 'onPostMetaChanged'), 10, 4);
+        add_action('added_post_meta', array($this, 'onPostMetaChanged'), 10, 4);
         add_action('updated_option', array($this, 'maybeAutoIncidentNumbersChanged'), 10, 3);
         add_action('updated_option', array($this, 'maybeIncidentNumberFormatChanged'), 10, 3);
         add_action('add_option_einsatzverwaltung_incidentnumbers_auto', array($this, 'onOptionAdded'), 10, 2);
@@ -34,22 +43,27 @@ class ReportNumberController
      * @param string $metaKey Der Key des postmeta-Eintrags
      * @param string $metaValue Der Wert des postmeta-Eintrags
      */
-    public function adjustIncidentNumber($metaId, $objectId, $metaKey, $metaValue)
+    public function onPostMetaChanged($metaId, $objectId, $metaKey, $metaValue)
     {
-        // Nur Änderungen an der laufenden Nummer sind interessant
-        if ('einsatz_seqNum' != $metaKey) {
+        // Bail, if this is not about reports
+        if (get_post_type($objectId) !== 'einsatz') {
             return;
         }
 
-        // Für den unwahrscheinlichen Fall, dass der Metakey bei anderen Beitragstypen verwendet wird, ist hier Schluss
-        $postType = get_post_type($objectId);
-        if ('einsatz' != $postType) {
-            return;
+        if ($metaKey === 'einsatz_seqNum' && self::isAutoIncidentNumbers()) {
+            $this->adjustIncidentNumber($objectId, $metaValue);
         }
+    }
 
-        $date = date_create(get_post_field('post_date', $objectId));
-        $newIncidentNumber = $this->formatEinsatznummer(date_format($date, 'Y'), $metaValue);
-        update_post_meta($objectId, 'einsatz_incidentNumber', $newIncidentNumber);
+    /**
+     * @param int $postId
+     * @param string $sequenceNumber
+     */
+    private function adjustIncidentNumber($postId, $sequenceNumber)
+    {
+        $date = date_create(get_post_field('post_date', $postId));
+        $newIncidentNumber = $this->formatEinsatznummer(date_format($date, 'Y'), $sequenceNumber);
+        update_post_meta($postId, 'einsatz_incidentNumber', $newIncidentNumber);
     }
 
     /**

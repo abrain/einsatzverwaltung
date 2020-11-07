b4cf944ddeb8969433cb55c6a22a0941eb293ce9
Andreas Brain <dev@abrain.de>
2020-11-07 22:14:56 +0100

    Add a few simple tests for the CSV source

---
+++ tests/unit/Import/Sources/CsvTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace abrain\Einsatzverwaltung\Import\Sources;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CsvTest
+ * @covers \abrain\Einsatzverwaltung\Import\Sources\AbstractSource
+ * @covers \abrain\Einsatzverwaltung\Import\Sources\Csv
+ * @package abrain\Einsatzverwaltung\Import\Sources
+ */
+class CsvTest extends TestCase
+{
+    public function testHasAnIdentifier()
+    {
+        $source = new Csv();
+        $identifier = $source->getIdentifier();
+        $this->assertIsString($identifier);
+        $this->assertNotEmpty($identifier);
+    }
+
+    public function testHasAName()
+    {
+        $source = new Csv();
+        $name = $source->getName();
+        $this->assertIsString($name);
+        $this->assertNotEmpty($name);
+    }
+
+    public function testHasADescription()
+    {
+        $source = new Csv();
+        $description = $source->getDescription();
+        $this->assertIsString($description);
+        $this->assertNotEmpty($description);
+    }
+}

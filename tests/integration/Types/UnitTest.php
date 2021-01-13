d8ae04f52dc700b8c568e3f24ae8147afe828fd7
Andreas Brain <dev@abrain.de>
2021-01-13 22:21:42 +0100

    Fix integration tests

--- tests/integration/Types/UnitTest.php
+++ tests/integration/Types/UnitTest.php
@@ -2,34 +2,13 @@
 namespace abrain\Einsatzverwaltung\Types;
 
 use PHPUnit_Framework_TestCase;
-use function array_map;
-use function get_post_meta;
-use function get_post_type_object;
-use function wp_delete_post;
-use function wp_insert_post;
+use function get_taxonomy;
 
 class UnitTest extends PHPUnit_Framework_TestCase
 {
     public function testTypeExists()
     {
-        $postType = get_post_type_object(Unit::getSlug());
-        $this->assertNotNull($postType);
-    }
-
-    public function testRemoveReferencesWhenUnitGetsDeleted()
-    {
-        // Associate a report with two units
-        $reportId = wp_insert_post(array('post_type' => 'einsatz', 'post_title' => 'Some Report'));
-        $unit1Id = wp_insert_post(array('post_type' => 'evw_unit', 'post_title' => 'Unit 1', 'post_status' => 'publish'));
-        $unit2Id = wp_insert_post(array('post_type' => 'evw_unit', 'post_title' => 'Unit 2', 'post_status' => 'publish'));
-        add_post_meta($reportId, '_evw_unit', $unit1Id);
-        add_post_meta($reportId, '_evw_unit', $unit2Id);
-
-        // Delete one unit
-        wp_delete_post($unit2Id);
-
-        // The report should no longer have a reference to the deleted unit
-        $unitIds = array_map('intval', get_post_meta($reportId, '_evw_unit'));
-        $this->assertEquals(array($unit1Id), $unitIds);
+        $taxonomy = get_taxonomy(Unit::getSlug());
+        $this->assertNotNull($taxonomy);
     }
 }

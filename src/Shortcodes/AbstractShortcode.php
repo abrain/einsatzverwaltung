a4794c57a7d0e4cac2483bed562d394d88d4540c
Andreas Brain <dev@abrain.de>
2021-01-21 01:22:58 +0100

    Make shortcodes with units backwards compatible by translating the unit IDs

--- src/Shortcodes/AbstractShortcode.php
+++ src/Shortcodes/AbstractShortcode.php
@@ -2,6 +2,11 @@
 
 namespace abrain\Einsatzverwaltung\Shortcodes;
 
+use abrain\Einsatzverwaltung\Types\Unit;
+use function array_key_exists;
+use function array_map;
+use const ARRAY_A;
+
 /**
  * Base class for Shortcode renderers that provides common functions
  *
@@ -35,5 +40,40 @@ abstract class AbstractShortcode
         $integers = array_filter($integers, 'is_numeric');
 
         return array_map('intval', $integers);
+    }
+
+    /**
+     * Translates IDs of old Units to the IDs of the new Units.
+     *
+     * @param int[] $idsToTranslate
+     *
+     * @return int[]
+     */
+    protected function translateOldUnitIds(array $idsToTranslate): array
+    {
+        global $wpdb;
+
+        if (empty($idsToTranslate)) {
+            return $idsToTranslate;
+        }
+
+        // Get a mapping of old and new IDs from the database
+        $results = $wpdb->get_results($wpdb->prepare(
+            "SELECT term_id, meta_value FROM $wpdb->termmeta WHERE term_id IN (SELECT term_id FROM $wpdb->term_taxonomy WHERE taxonomy = %s) AND meta_key = %s",
+            Unit::getSlug(),
+            'old_unit_id'
+        ), ARRAY_A);
+        if (empty($results)) {
+            return $idsToTranslate;
+        }
+
+        $map = [];
+        foreach ($results as $result) {
+            $map[$result['meta_value']] = $result['term_id'];
+        }
+
+        return array_map(function ($idToTranslate) use ($map) {
+            return array_key_exists($idToTranslate, $map) ? $map[$idToTranslate] : $idToTranslate;
+        }, $idsToTranslate);
     }
 }

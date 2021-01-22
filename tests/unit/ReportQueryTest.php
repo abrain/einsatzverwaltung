5a3956b347e8a2690cdbd80fead57a326458b957
Andreas Brain <dev@abrain.de>
2021-01-22 19:52:12 +0100

    Code style

--- tests/unit/ReportQueryTest.php
+++ tests/unit/ReportQueryTest.php
@@ -1,6 +1,9 @@
 <?php
 namespace abrain\Einsatzverwaltung;
 
+use abrain\Einsatzverwaltung\Model\IncidentReport;
+use Brain\Monkey\Expectation\Exception\ExpectationArgsRequired;
+use Brain\Monkey\Expectation\Exception\NotAllowedMethod;
 use Mockery;
 use function Brain\Monkey\Functions\expect;
 
@@ -13,6 +16,10 @@ use function Brain\Monkey\Functions\expect;
  */
 class ReportQueryTest extends UnitTestCase
 {
+    /**
+     * @throws ExpectationArgsRequired
+     * @throws NotAllowedMethod
+     */
     public function testGetReports()
     {
         $reportQuery = new ReportQuery();
@@ -20,59 +27,74 @@ class ReportQueryTest extends UnitTestCase
         $post1->ID = 45;
         $post2 = Mockery::mock('\WP_Post');
         $post2->ID = 56;
-        expect('get_posts')->once()->with(Mockery::type('array'))->andReturn(array($post1, $post2));
+        expect('get_posts')->once()->with(Mockery::type('array'))->andReturn([$post1, $post2]);
         expect('get_post_type')->twice()->andReturn('einsatz');
         expect('get_post')->twice()->andReturnFirstArg();
         $incidentReports = $reportQuery->getReports();
 
         $this->assertCount(2, $incidentReports);
         foreach ($incidentReports as $incidentReport) {
-            $this->assertInstanceOf('\abrain\Einsatzverwaltung\Model\IncidentReport', $incidentReport);
+            $this->assertInstanceOf(IncidentReport::class, $incidentReport);
         }
         $this->assertEquals($post1->ID, $incidentReports[0]->getPostId());
         $this->assertEquals($post2->ID, $incidentReports[1]->getPostId());
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetOrderAsc()
     {
         $reportQuery = new ReportQuery();
         $reportQuery->setOrderAsc(true);
         expect('get_posts')->once()->with(Mockery::on(function ($array) {
             return is_array($array) && $array['order'] == 'ASC';
-        }))->andReturn(array());
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetExcludePostIds()
     {
         $reportQuery = new ReportQuery();
-        $reportQuery->setExcludePostIds(array('5', '13', '26'));
+        $reportQuery->setExcludePostIds(['5', '13', '26']);
         expect('get_posts')->once()->with(Mockery::on(function ($array) {
-            return is_array($array) && $array['post__not_in'] == array('5', '13', '26');
-        }))->andReturn(array());
+            return is_array($array) && $array['post__not_in'] == ['5', '13', '26'];
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetYear()
     {
         $reportQuery = new ReportQuery();
         $reportQuery->setYear(2022);
         expect('get_posts')->once()->with(Mockery::on(function ($array) {
             return is_array($array) && $array['date_query']['year'] == 2022;
-        }))->andReturn(array());
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetYearToZero()
     {
         $reportQuery = new ReportQuery();
         $reportQuery->setYear(0);
         expect('get_posts')->once()->with(Mockery::on(function ($array) {
             return is_array($array) && is_array($array['date_query']) && empty($array['date_query']);
-        }))->andReturn(array());
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetYearToNegative()
     {
         $reportQuery = new ReportQuery();
@@ -83,25 +105,31 @@ class ReportQueryTest extends UnitTestCase
             return is_array($array) && !array_key_exists('year', $array['date_query']) &&
                 count($array['date_query']) === 4 &&
                 $array['date_query']['relation'] === 'OR' &&
-                in_array(array('year' => $currentYear), $array['date_query']) &&
-                in_array(array('year' => $currentYear - 1), $array['date_query']) &&
-                in_array(array('year' => $currentYear - 2), $array['date_query']);
-        }))->andReturn(array());
+                in_array(['year' => $currentYear], $array['date_query']) &&
+                in_array(['year' => $currentYear - 1], $array['date_query']) &&
+                in_array(['year' => $currentYear - 2], $array['date_query']);
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetUnits()
     {
         $reportQuery = new ReportQuery();
-        $reportQuery->setUnits(array(146,7544));
+        $reportQuery->setUnits([146,7544]);
         expect('get_posts')->once()->with(Mockery::on(function ($array) {
             return is_array($array) && array_key_exists('tax_query', $array) && is_array($array['tax_query']) &&
                 (!array_key_exists('relation', $array['tax_query']) || $array['tax_query']['relation'] === 'AND') &&
-                in_array(array('taxonomy' => 'evw_unit', 'terms' => [146, 7544]), $array['tax_query']);
-        }))->andReturn(array());
+                in_array(['taxonomy' => 'evw_unit', 'terms' => [146, 7544]], $array['tax_query']);
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetIncidentTypeId()
     {
         $reportQuery = new ReportQuery();
@@ -109,11 +137,14 @@ class ReportQueryTest extends UnitTestCase
         expect('get_posts')->once()->with(Mockery::on(function ($array) {
             return is_array($array) && array_key_exists('tax_query', $array) && is_array($array['tax_query']) &&
                 (!array_key_exists('relation', $array['tax_query']) || $array['tax_query']['relation'] === 'AND') &&
-                in_array(array('taxonomy' => 'einsatzart', 'terms' => array(951)), $array['tax_query']);
-        }))->andReturn(array());
+                in_array(['taxonomy' => 'einsatzart', 'terms' => [951]], $array['tax_query']);
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetIncludePrivateReports()
     {
         $reportQuery = new ReportQuery();
@@ -121,10 +152,13 @@ class ReportQueryTest extends UnitTestCase
         expect('get_posts')->once()->with(Mockery::on(function ($array) {
             return is_array($array) && in_array('publish', $array['post_status']) &&
                 in_array('private', $array['post_status']);
-        }))->andReturn(array());
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetOnlySpecialReports()
     {
         $reportQuery = new ReportQuery();
@@ -132,11 +166,14 @@ class ReportQueryTest extends UnitTestCase
         expect('get_posts')->once()->with(Mockery::on(function ($array) {
             return is_array($array) && array_key_exists('meta_query', $array) && is_array($array['meta_query']) &&
                 (!array_key_exists('relation', $array['meta_query']) || $array['meta_query']['relation'] === 'AND') &&
-                in_array(array('key' => 'einsatz_special', 'value' => '1'), $array['meta_query']);
-        }))->andReturn(array());
+                in_array(['key' => 'einsatz_special', 'value' => '1'], $array['meta_query']);
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testSetLimit()
     {
         $reportQuery = new ReportQuery();
@@ -146,7 +183,7 @@ class ReportQueryTest extends UnitTestCase
                 (array_key_exists('numberposts', $array) && $array['numberposts'] === 63) ||
                 (array_key_exists('posts_per_page', $array) && $array['posts_per_page'] === 63)
             );
-        }))->andReturn(array());
+        }))->andReturn([]);
         $reportQuery->getReports();
     }
 }

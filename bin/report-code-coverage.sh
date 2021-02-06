1dd1c2e3e35e34e0c3a8d9cf101eeed4968b0706
Andreas Brain <dev@abrain.de>
2021-02-06 16:22:23 +0100

    Apply the changes made in feature/refactor_import without merging them

--- bin/report-code-coverage.sh
+++ bin/report-code-coverage.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# The root directory of the project is one up
+cd "$(dirname "$0")/.."
+
 # Set the environment variable GIT_BRANCH for the Code Climate test-reporter, so it does not report the coverage for the
 # target branch of pull requests but for the source branch
 if [ "$DRONE_BUILD_EVENT" == "pull_request" ]; then
@@ -7,3 +10,5 @@ if [ "$DRONE_BUILD_EVENT" == "pull_request" ]; then
 else
     export GIT_BRANCH="$DRONE_COMMIT_BRANCH"
 fi
+
+./cc-test-reporter after-build --debug --coverage-input-type clover

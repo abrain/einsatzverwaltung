acf200851df27ccbb3b2b069531c050a1f1c155e
Andreas Brain <dev@abrain.de>
2021-10-22 11:35:27 +0200

    Update unit test for role updates

--- tests/unit/UserRightsManagerTest.php
+++ tests/unit/UserRightsManagerTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace abrain\Einsatzverwaltung;
 
+use Brain\Monkey\Expectation\Exception\ExpectationArgsRequired;
 use Mockery;
 use function Brain\Monkey\Functions\expect;
 
@@ -11,8 +12,37 @@ use function Brain\Monkey\Functions\expect;
  */
 class UserRightsManagerTest extends UnitTestCase
 {
+    /**
+     * @throws ExpectationArgsRequired
+     */
     public function testUpdateRoles()
     {
+        $adminRole = Mockery::mock('\WP_Role');
+        $adminRole->expects('add_cap')->once()->with('edit_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('edit_private_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('edit_published_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('edit_others_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('publish_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('read_private_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('delete_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('delete_private_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('delete_published_einsatzberichte');
+        $adminRole->expects('add_cap')->once()->with('delete_others_einsatzberichte');
+
+        expect('get_role')->once()->with('administrator')->andReturn($adminRole);
+
+        expect('remove_role')->atLeast()->once()->with(Mockery::type('string'));
+        expect('add_role')->atLeast()->once()->with(Mockery::type('string'), Mockery::type('string'), Mockery::type('array'));
+        (new UserRightsManager())->updateRoles();
+    }
+
+    /**
+     * @throws ExpectationArgsRequired
+     */
+    public function testRoleUpdateToleratesMissingAdminRole()
+    {
+        expect('get_role')->once()->with('administrator')->andReturn(false);
+
         expect('remove_role')->atLeast()->once()->with(Mockery::type('string'));
         expect('add_role')->atLeast()->once()->with(Mockery::type('string'), Mockery::type('string'), Mockery::type('array'));
         (new UserRightsManager())->updateRoles();
